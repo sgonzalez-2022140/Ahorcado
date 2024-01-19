ad70a487131b15db01ddc0235b4aeccf4a1bc8f9
Informatica <informatica@alumno.local>
2024-01-19 08:27:19 -0600

    3ra actualizacion

---
+++ 3ra actualizacion/script.js
@@ -0,0 +1,30 @@
+
+// Elegir palabras al azar 
+const palabras = [`Agucate`,`Mono`,`Llorona`,`Sujeto`,`Lavadora`];
+const aleatorio = palabras[Math.floor(Math.floor(Math.random() * palabras.length))];
+
+document.getElementById(`palabra`).value=aleatorio;
+
+
+
+console.log(aleatorio);
+
+// graficar
+
+var lienzo = document.getElementById("lienzo");
+		if (lienzo && lienzo.getContext) {
+		var contexto = lienzo.getContext("2d");
+			if (contexto) {
+					var X = lienzo.width/2;
+					var Y = lienzo.height/2;
+					var r = 75;
+					contexto.strokeStyle = "#A57E6E";
+					contexto.fillStyle = "#D2A18C";
+					contexto.lineWidth = 5;
+					contexto.arc(X,Y,r,0,2*Math.PI);
+					contexto.fill();
+					contexto.stroke();
+			}
+		}
+
+
